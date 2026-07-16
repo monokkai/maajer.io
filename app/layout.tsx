4b572a4ff1d81fc30736ad268319593db8415329
monokkai <egormayer08@mail.ru>
2026-07-16 21:07:59 +0200

    feat: analytics from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import Footer from "./components/footer";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
